fbe5871cd881aa234cc129d1a31b1f93cf889723
R0N1n-dev <mdrewj@live.com>
2021-12-23 14:45:51 +0300

    Initial deploy
    added deps

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,7 @@
 import Vue from "vue";
 import "bulma/css/bulma.css";
-import "vue-awesome/icons";
-import Icon from "vue-awesome/components/Icon";
 
 import App from "./App.vue";
-
-Vue.component("v-icon", Icon);
 
 Vue.config.productionTip = false;
 
